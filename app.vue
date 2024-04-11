9fa6aaa3317e3f28eeb353b70c947b98b06f625b
Armen Rostamian <armenr@gmail.com>
2024-04-11 12:25:21 +0400

    fix(SSR): Instantiate taskSTore onBeforeMount of main app.vue to avoid hydration mismatch with SSR

--- app.vue
+++ app.vue
@@ -1,4 +1,10 @@
-<script setup></script>
+<script setup lang="ts">
+onBeforeMount(() => {
+  console.log('In Nuxt, you can think of a Pinia store like a composable function. The state will not be initialized until you invoke it explicitly.');
+  console.log('This is why we need to call useTaskStore() in the APP.vue, to avoid Hydration mismatch errors with SSR.')
+  useTaskStore()
+});
+</script>
 
 <template>
   <NuxtLayout>
